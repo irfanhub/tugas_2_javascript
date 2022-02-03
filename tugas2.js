701339984098686278479efe5267518378d171d4
Irfan <irfannsuprianto@gmail.com>
2022-02-03 22:35:51 +0700

    tugas 2 javascript
    tugas ini saya kumpulkan untuk niomic

---
+++ tugas2.js
@@ -0,0 +1,16 @@
+var number = 21;
+// tipe data number
+var bolean = true;
+//tipe data bolean
+var string = "irfan";
+// tipe data string
+var data = null;
+// tipe data null
+var data2; 
+//tipe data undifined
+
+console.log(number);
+console.log(bolean);
+console.log(string);
+console.log(data);
+console.log(data2);
